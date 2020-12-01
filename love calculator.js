c69c82533e1592db9150a17a0de1739ce62e41f2
krishnarudani <krishnarudani062@gmail.com>
2020-12-01 12:29:54 +0530

    first commit

---
+++ love calculator.js
@@ -0,0 +1,20 @@
+prompt("what is your name?");
+prompt("what is your name?");
+
+var loveScore=Math.random()*100;
+lovescore=Math.floor(loveScore)+1;
+
+if(lovescore>80){
+  alert("your love score is"+" "+lovescore+"%"+"you made for each other");
+}
+
+if(lovescore=>30 && lovescore=80;){
+  alert("your love score is"+" "+lovescore+"%"+);
+}
+
+if(lovescore>=30){
+  alert("your love score is"+" "+lovescore +"%"+"you go together oil and water");    
+}
+  else{
+    alert("your love score is"+" "+lovescore+"%"+);
+  }
