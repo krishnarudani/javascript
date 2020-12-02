f5f083dbcc863c451317365537d149fb53d02d17
krishnarudani <krishnarudani062@gmail.com>
2020-12-02 12:23:44 +0530

    add while loop

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -2,18 +2,21 @@ var output=[];
 var count=1;
 function fizzBuzz() {
     
-    if(count % 3 === 0 && count % 5 === 0){
-        output.push("FizzBuzz");
+    while(count <=100 ){
+        if(count % 3 === 0 && count % 5 === 0){
+            output.push("FizzBuzz");
+        }
+         if(count % 3 === 0){
+            output.push("Fizz");
+        }
+        else if(count % 5 === 0){
+            output.push("Buzz");
+        }
+        else{
+            output.push(count);
+        }
+        count++;
     }
-     if(count % 3 === 0){
-        output.push("Fizz");
-    }
-    else if(count % 5 === 0){
-        output.push("Buzz");
-    }
-    else{
-        output.push(count);
-    }
-    count++;
+    
     console.log(output);
 } 
