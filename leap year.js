039002bdff814095d06dceded5745688d3d36ace
krishnarudani <krishnarudani062@gmail.com>
2020-12-02 10:32:52 +0530

    cousmetic changes

---
+++ leap year.js
@@ -0,0 +1,17 @@
+function isleap(year){
+
+  if(year % 4 === 0){
+    if(year % 100 === 0){
+      if(year % 400 === 0){
+          return "leap year";
+      }
+      }else{
+        return "not leap year";
+      }
+    }else{
+      return "leap year";
+    }
+  }else{
+    return "not leap year";
+  }  
+}
